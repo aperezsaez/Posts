80a4748b3a4add09e42fe561f7f80b590b81df51
aperezsaez <alex.perez.22@live.com>
2021-08-26 15:36:29 -0400

    Add: Hashtags

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,13 @@
 module PostsHelper
+  def hashtag(content)
+    hash = content.split(" ")
+    words = []
+      hash.each do |twt|
+        if twt.start_with?("#")
+          twt = link_to twt, controller: "posts", q: twt
+        end
+        words.push(twt)
+      end
+      raw words.join(" ")
+  end
 end
